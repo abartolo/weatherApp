df35dfcc41782838dc457fa7db3a41856c139794
abartolo <abartolo2@gmail.com>
2015-04-23 20:27:30 -0400

    Preparing to add NodeJS to host

---
+++ web-server.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Alex on 4/23/2015.
+ * Description: This is should be the webserver for my NodeJS project.
+ *  Running "node web-server.js" should start the server and on localhost be able
+ *  to run my application
+ */
+
+
+
+
+
