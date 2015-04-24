e2061e74c8d1fe7892bffe2138215c3e0f9f77da
abartolo <abartolo2@gmail.com>
2015-04-23 21:29:24 -0400

    Completed nodejs webserver. Ready to run

--- web-server.js
+++ web-server.js
@@ -5,7 +5,50 @@
  *  to run my application
  */
 
+var express = require('express');
+var app = express();
+var path = require('path');
 
+
+
+//**********Routes*********
+//sends index.html when requested
+app.get('/',function(req,res){
+
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+
+});
+
+//sends homepage.html when requested
+app.get('/pages/homepage.html',function(req,res){
+
+    res.sendFile(path.join(__dirname + '/public/pages/homepage.html'));
+
+});
+//sends forecast.html when requested
+app.get('/pages/forecast.html',function(req,res){
+
+    res.sendFile(path.join(__dirname + '/public/pages/forecast.html'));
+
+});
+
+//sends forecastResultContainer.html when requested
+app.get('/directives/forecastResultContainer.html',function(req,res){
+
+    res.sendFile(path.join(__dirname + '/public/directives/forecastResultContainer.html'));
+
+});
+
+//sends app.js when requested
+app.get('/js/app.js',function(req,res){
+
+    res.sendFile(path.join(__dirname + '/public/js/app.js'));
+
+});
+
+app.listen(3000);
+
+console.log('NodeJS - Listening in port 3000');
 
 
 
